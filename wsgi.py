6364f1dfce868f6e31c38d7e1c7836982bae6759
VeilanceApp <320058092+VeilanceApp@users.noreply.github.com>
2026-08-27 21:17:41 +0000

    updates

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,9 @@ from flask_cors import CORS
 
 
 app = api.app
-app.register_blueprint(api.veilance_v1)
+app.register_blueprint(api.veilance_public_v1)
+app.register_blueprint(api.veilance_users_v1)
+app.register_blueprint(api.veilance_admin_v1)
 
 
 CORS(app, resources={r"/a*": {"origins": "*"}})
